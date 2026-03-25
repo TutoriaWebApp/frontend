2f5b22c870c8bdd96ee1f0e0f8eca93f569e19b3
Lucas de Lima Spinosa <lucasspinosa.software@gmail.com>
2026-03-25 00:17:48 -0300

    Mudança de formulário para RHF

--- apps/web/app/redefinir-senha/[uid]/[token]/page.tsx
+++ apps/web/app/redefinir-senha/[uid]/[token]/page.tsx
@@ -3,19 +3,52 @@
 import React from "react";
 
 import { useState, useContext } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useParams } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
 import { NotificationContext } from "@repo/ui/contexts/NotificationContext/NotificationContext";
+import {validateJwt} from "@repo/lib/jwtAux"
+
 import VisibilityIcon from "@mui/icons-material/Visibility";
 
-export default function ForgotPassword(): React.ReactNode {
-  const [newPassword, setNewPassword] = useState<string>("");
-  const [confirmNewPassword, setConfirmNewPassword] = useState<string>("");
-  const [requirements, setRequirements] = useState({
-    length: false,
-    special: false,
-    number: false,
-    all: false,
+const registerSchema = z
+  .object({
+    password: z
+      .string()
+      .min(10, "A senha deve possuir 10 ou mais caracteres.")
+      .regex(/[!@#$%^&*]/, "A senha deve conter um caractere especial.")
+      .regex(/[0-9]/, "A senha deve conter pelo menos um número."),
+
+    passwordConfirm: z
+      .string()
+      .min(10, "A senha deve possuir 10 ou mais caracteres.")
+      .regex(/[!@#$%^&*]/, "A senha deve conter um caractere especial.")
+      .regex(/[0-9]/, "A senha deve conter pelo menos um número."),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "As senhas devem ser iguais.",
+    path: ["passwordConfirm"], 
   });
+
+type RegisterData = z.infer<typeof registerSchema>;
+
+const onSubmit = async (data: RegisterData) => {
+  console.log("Dados validados:", data);
+  // Aqui chamarias a tua Server Action ou API
+};
+
+export default function ForgotPassword(): React.ReactNode {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterData>({
+    resolver: zodResolver(registerSchema),
+    mode: "onChange"
+  });
+
   const [showNewPassword, setShowNewPassword] = useState<boolean>(false);
   const [showConfirmNewPassword, setShowConfirmNewPassword] =
     useState<boolean>(false);
@@ -24,38 +57,23 @@ export default function ForgotPassword(): React.ReactNode {
 
   const { showNotification } = useContext(NotificationContext);
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
+  // const handleSubmit = (e: any) => {
+  //   e.preventDefault();
 
-    if (newPassword != confirmNewPassword) {
-      showNotification("As senhas nos dois campos devem ser iguais!", "error");
-      return;
-    } else if (newPassword == "" || confirmNewPassword == "") {
-      showNotification("Informe a senha nos dois campos!", "error");
-      return;
-    } else if (!requirements.all) {
-      showNotification("A senha não atende à todos os critérios!", "error");
-      return;
-    } else {
-      showNotification("Senha redefinida com sucesso!", "success");
-      router.push("/");
-    }
-  };
-
-  const checkPassword = (password: string) => {
-    const isLengthOk = /.{10,}/.test(password);
-    const isSpecialOk = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-    const isNumberOk = /[0-9]/.test(password);
-
-    setNewPassword(password);
-
-    setRequirements({
-      length: isLengthOk,
-      special: isSpecialOk,
-      number: isNumberOk,
-      all: isLengthOk && isSpecialOk && isNumberOk,
-    });
-  };
+  //   if (newPassword != confirmNewPassword) {
+  //     showNotification("As senhas nos dois campos devem ser iguais!", "error");
+  //     return;
+  //   } else if (newPassword == "" || confirmNewPassword == "") {
+  //     showNotification("Informe a senha nos dois campos!", "error");
+  //     return;
+  //   } else if (!requirements.all) {
+  //     showNotification("A senha não atende à todos os critérios!", "error");
+  //     return;
+  //   } else {
+  //     showNotification("Senha redefinida com sucesso!", "success");
+  //     router.push("/");
+  //   }
+  // };
 
   return (
     <div className="bg-slate-50">
@@ -64,21 +82,24 @@ export default function ForgotPassword(): React.ReactNode {
          bg-white 
          mx-auto
          relative
-         top-[15%]
+         md:top-[5%]
          rounded-3xl
          border 
          border-slate-200
          shadow-xl
        shadow-slate-200/50
          w-fit
+         2xl:top-[15%]
         "
       >
-        <div className="
+        <div
+          className="
           flex
           flex-col
           items-center
           mb-4
-          ">
+          "
+        >
           <h2 className="font-quicksand text-2xl font-bold m-5">
             Redefinição de Senha
           </h2>
@@ -92,15 +113,13 @@ export default function ForgotPassword(): React.ReactNode {
           pb-6
           "
         >
-          <form onSubmit={handleSubmit} className="flex flex-col">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
             <div className="flex items-center">
               <label className="flex flex-col pl-6">
                 <span className="font-semibold mb-2">Nova senha</span>
                 <input
                   type={showNewPassword ? "text" : "password"}
-                  name="password"
-                  value={newPassword}
-                  onChange={(e) => checkPassword(e.target.value)}
+                  {...register("password")}
                   className="
                 bg-white  
                 w-[470px]
@@ -122,36 +141,11 @@ export default function ForgotPassword(): React.ReactNode {
               </div>
             </div>
             <div className="mb-6 pl-6 mt-4 flex flex-col">
-              <span
-                className={
-                  requirements.all ? "text-green-500" : "text-rose-500"
-                }
-              >
-                A senha deve possuir:
-              </span>
-              <ul className="list-disc pl-4">
-                <li
-                  className={
-                    requirements.length ? "text-green-500" : "text-rose-500"
-                  }
-                >
-                  <span>10 ou mais caracteres;</span>
-                </li>
-                <li
-                  className={
-                    requirements.special ? "text-green-500" : "text-rose-500"
-                  }
-                >
-                  <span>1 caractere especial;</span>
-                </li>
-                <li
-                  className={
-                    requirements.number ? "text-green-500" : "text-rose-500"
-                  }
-                >
-                  <span>1 número.</span>
-                </li>
-              </ul>
+              {errors.password && (
+                <span className="text-rose-500 text-sm mt-1">
+                  {errors.password.message}
+                </span>
+              )}
             </div>
             <div className="flex items-center">
               <label className="flex flex-col pl-6">
@@ -160,9 +154,7 @@ export default function ForgotPassword(): React.ReactNode {
                 </span>
                 <input
                   type={showConfirmNewPassword ? "text" : "password"}
-                  name="password"
-                  value={confirmNewPassword}
-                  onChange={(e) => setConfirmNewPassword(e.target.value)}
+                  {...register("passwordConfirm")}
                   className="
                 bg-white  
                 w-[470px]
@@ -185,11 +177,13 @@ export default function ForgotPassword(): React.ReactNode {
                 Mostrar
               </div>
             </div>
-            {newPassword != confirmNewPassword && (
-              <div className="mt-6 text-rose-500 pl-6">
-                <span>As senhas devem ser iguais.</span>
-              </div>
-            )}
+            <div className="pl-6 mt-4 flex flex-col">
+              {errors.passwordConfirm && (
+                <span className="text-rose-500 text-sm mt-1">
+                  {errors.passwordConfirm.message}
+                </span>
+              )}
+            </div>
             <button
               className="
               bg-indigo-600
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    env: {
+        backendBaseURL: 'http://localhost:8000/v1'
+    }
 };
 
 export default nextConfig;
