74e85ae90919218e6b063ed60a465af7db837bf8
Lucas de Lima Spinosa <lucasspinosa.software@gmail.com>
2026-04-07 22:33:21 -0300

    Botão de Editar Perfil

---
+++ packages/ui/src/EditProfileButton/EditProfileButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { redirect } from "next/navigation";
+
+export const EditProfileButton = () => {
+    const redirectToEdit = () => {
+        redirect("/editar-perfil")
+    }
+
+  return (
+    <>
+      <button
+      onClick={redirectToEdit}
+        className="
+        bg-brand-primary 
+        hover:bg-indigo-800 
+        text-white 
+        font-bold 
+        py-2.5 
+        px-6 
+        rounded-xl 
+        transition-all 
+        shadow-lg 
+        shadow-brand-primary/20
+        "
+      >
+        Editar Perfil
+      </button>
+    </>
+  );
+};
