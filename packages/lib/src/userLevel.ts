320b12dcd19cef2334394f6ed53cf733f202b1a3
Lucas de Lima Spinosa <lucasspinosa.software@gmail.com>
2026-09-07 19:43:11 -0300

    Ajuste de XPs de Níveis

--- packages/lib/src/userLevel.ts
+++ packages/lib/src/userLevel.ts
@@ -1,10 +1,19 @@
 // Lista de XP Total Acumulado extraída da sua Tabela de Progressão
 export const levelThresholds = [
-  100, 201, 302, 403, 504, 705, 1006, 1407, 1908, 2509, // Níveis 1-10 
-  3210, 4011, 4912, 5913, 7014, 8215, 9516, 10917, 12418, 14019, // Níveis 11-20 
-  15720, 17521, 19422, 21423, 23524, 25725, 28026, 30427, 32928, 35529, // Níveis 21-30 
-  38230, 41031, 43932, 46933, 50034, 53235, 56536, 59937, 63438, 67039, // Níveis 31-40 
-  70740, 74541, 78442, 82443, 86544, 90745, 95046, 99447, 103948, 108549 // Níveis 41-50 
+  // Níveis 1-10: Onboarding Rápido (Primeiras mensagens, perfil e primeiras aulas)
+  100, 250, 450, 700, 1000, 1350, 1750, 2200, 2700, 3250,
+
+  // Níveis 11-20: Engajamento Médio (Conquistas de Prata e 15 a 30 aulas)
+  3850, 4500, 5200, 5950, 6750, 7600, 8500, 9450, 10450, 11500,
+
+  // Níveis 21-30: Usuário Avançado (Início das conquistas de Ouro e 50+ aulas)
+  12600, 13750, 14950, 16200, 17500, 18850, 20250, 21700, 23200, 24750,
+
+  // Níveis 31-40: Veterano (Conquistas difíceis e 100+ aulas)
+  26350, 28000, 29700, 31450, 33250, 35100, 37000, 38950, 40950, 43000,
+
+  // Níveis 41-50: Grind Supremo (Quase 100% de conquistas + 200-300 aulas)
+  45100, 47250, 49450, 51700, 54000, 56350, 58750, 61200, 63700, 66250
 ];
 
 export const userLevel = (xp: number): number => {
