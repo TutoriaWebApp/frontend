c5640cf02d0ec11b6b82c6552e4f08f4219b2baf
Lucas de Lima Spinosa <lucasspinosa.software@gmail.com>
2026-09-11 22:16:25 -0300

    Adição de mais endpoints de Cnquista

--- packages/services/src/achievements.ts
+++ packages/services/src/achievements.ts
@@ -1,4 +1,4 @@
-import {GetAllAchievementsResult, GetUserAchievementsResult} from "./types/achievements"
+import {GetAllAchievementsResult, GetUserAchievementsResult, AchivementUnlockedResult, GetSpecificAchievementsResult} from "./types/achievements"
 import { authRequestWrapper } from "@repo/lib/authRequestWrapper"
 import { getBackendUrl } from "@repo/lib/getBackendUrl"
 
@@ -13,6 +13,32 @@ export async function GetAllAchievements(): Promise<GetAllAchievementsResult> {
         headers: { "Content-Type": "application/json" },
       },
       "Request All Achievements",
+    );
+
+    if (res.success) {
+      return {
+        success: true,
+        status: res.status,
+        data: res.data,
+      };
+    }
+    return { success: false, status: res.status };
+  } catch (e) {
+    return { success: false, status: 500 };
+  }
+}
+
+export async function GetSpecificAchievement(achievementId: number): Promise<GetSpecificAchievementsResult> {
+  const URL = `${process.env.backendBaseURL}/conquistas/${achievementId}/`;
+
+  try {
+    const res = await authRequestWrapper(
+      URL,
+      {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      },
+      "Request Specific Achievements",
     );
 
     if (res.success) {
@@ -51,5 +77,50 @@ export async function GetUserAchievements(userId: number): Promise<GetUserAchiev
     return { success: false, status: res.status };
   } catch (e) {
     return { success: false, status: 500 };
+  }
+}
+
+export async function UnlockAchievement(
+  userId: number,
+  achievementId: number,
+  // cookieString: string,
+  // csrfTokenString: string | undefined,
+): Promise<AchivementUnlockedResult> {
+  const URL = `${process.env.backendBaseURL}/consegue/`;
+
+  try {
+    const res = await authRequestWrapper(
+      URL,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          // Cookie: cookieString,
+          // "X-CSRFToken": csrfTokenString,
+        },
+        body: JSON.stringify({usuarioId: userId, conquistaId: achievementId}),
+      },
+      "Post Unlock Achiwvement",
+    );
+
+    if (res.success) {
+      return {
+        success: res.success,
+        status: res.status,
+      };
+    } else {
+      return {
+        success: false,
+        status: res.status,
+      };
+    }
+  } catch (e) {
+    console.error("Post Unlock Achievement Request Error:", e);
+
+    return {
+      success: false,
+      status: 500,
+    };
   }
 }
